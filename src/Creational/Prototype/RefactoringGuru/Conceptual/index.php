b9ac8171ba2dd78757920ccb6002815c3d36c42a
Sline-X <kipa.maksim99@gmail.com>
2023-08-31 23:15:55 +0300

    feat(Prototype): pattern Prototype conceptual

--- src/Creational/Prototype/RefactoringGuru/Conceptual/index.php
+++ src/Creational/Prototype/RefactoringGuru/Conceptual/index.php
@@ -34,5 +34,52 @@ class Prototype
 
 class ComponentWithBackReference
 {
+    public $prototype;
 
+    /**
+     * Обратите внимание, что конструктор не будет выполнен во время
+     * клонирования. Если у вас сложная логика внутри конструктора, вам может
+     * потребоватьься выполнить е также и в методе clone.
+     */
+    public function __construct(Prototype $prototype)
+    {
+        $this->prototype = $prototype;
+    }
 }
+
+/**
+ * Клиентский код
+ */
+function clientCode()
+{
+    $p1 = new Prototype();
+    $p1->primitive = 245;
+    $p1->component = new \DateTime();
+    $p1->circularReference = new ComponentWithBackReference($p1);
+
+    $p2 = clone $p1;
+    if ($p1->primitive === $p2->primitive) {
+        echo "Primitive field values have been carried over to a clone. Yay!\n";
+    } else {
+        echo "Primitive field value have not been copied. Booo!\n";
+    }
+    if ($p1->component === $p2->component) {
+        echo "Simple component has not been cloned. Booo!\n";
+    } else {
+        echo "Simple component has been cloned. Yay!\n";
+    }
+
+    if ($p1->circularReference === $p2->circularReference) {
+        echo "Component with back reference has not been cloned. Booo!\n";
+    } else {
+        echo "Component with back reference has been cloned. Yay!\n";
+    }
+
+    if ($p1->circularReference->prototype === $p2->circularReference->prototype) {
+        echo "Component with back reference is linked to original object. Booo!\n";
+    } else {
+        echo "Component with back reference is linked to the clone. Yay!\n";
+    }
+}
+
+clientCode();
